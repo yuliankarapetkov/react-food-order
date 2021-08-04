d0b421775497dbf63a2e48f307079e57846d83c7
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2021-08-04 16:00:40 +0300

    feat: add an animation to CartButton

--- src/components/Layout/CartButton.js
+++ src/components/Layout/CartButton.js
@@ -1,17 +1,31 @@
 import CartContext from '../../store/CartContext';
 import CartIcon from '../Shared/CartIcon';
-import { useContext } from 'react';
 import styles from './CartButton.module.css';
+import { useContext, useEffect, useState } from 'react';
 
 const CartButton = ({ onClick }) => {
+    const [buttonHiglighted, setButtonHighlighted] = useState(false);
+
     const { items } = useContext(CartContext);
 
+    useEffect(() => {
+        if (!items.length) return;
+
+        setButtonHighlighted(true);
+
+        const timer = setTimeout(() => setButtonHighlighted(false), 300);
+
+        return () => clearTimeout(timer);
+    }, [items]);
+
     const itemsCount = items.reduce((count, item) => count + item.amount, 0);
+
+    const buttonStyles = `${styles.button} ${buttonHiglighted ? styles.bump : ''}`;
 
     return (
         <button
             type="button"
-            className={styles.button}
+            className={buttonStyles}
             onClick={onClick}
         >
             <span className={styles.icon}>
